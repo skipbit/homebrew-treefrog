ca3d6b1d0057dd0161cdc248e42d52aeb6d615da
Yuma Endo <endo@skipbit.jp>
2016-11-11 18:21:29 +0900

    Reflect review of mainline.

--- treefrog.rb
+++ treefrog.rb
@@ -1,39 +1,32 @@
 class Treefrog < Formula
   desc "High-speed C++ MVC Framework for Web Application"
   homepage "http://www.treefrogframework.org/"
-  url "https://github.com/treefrogframework/treefrog-framework.git", :tag => "v1.13.0", :revision => "dad8d4bbb4ae0bfb480352fde3ba5dd1321ccff8"
+  url "https://github.com/treefrogframework/treefrog-framework/archive/v1.13.0.tar.gz"
+  sha256 "850744fef5254a8b2de1d949ba3490514ce1095e9a3e81da40aa8160129e14a1"
   head "https://github.com/treefrogframework/treefrog-framework.git", :branch => "master"
 
-  option "with-debug", "build with debug information"
-  option "with-gui", "build and link with QtGui module"
-  option "with-mongo", "build with MongoDB driver library"
+  bottle do
+    sha256 "8d5daeb95fabb1bc5d0821cd4a29414b1cbc2d5483b094dd8d8a435172028685" => :sierra
+    sha256 "8d5daeb95fabb1bc5d0821cd4a29414b1cbc2d5483b094dd8d8a435172028685" => :el_capitan
+  end
+
   option "with-mysql", "enable --with-mysql option for Qt build"
   option "with-oci", "enable --with-oci option for Qt build"
   option "with-postgresql", "enable --with-postgresql option for Qt build"
+  option "with-qt5", "build and link with QtGui module"
 
-  depends_on MinimumMacOSRequirement => :el_capitan
-  depends_on XcodeRequirement => [:build, :version => ">= 8.0"]
+  depends_on :macos => :el_capitan
+  depends_on :xcode => [:build, "8.0"]
 
-  qtopts = []
-  qtopts << "with-oci" if build.with?("oci") && ENV["ORACLE_HOME"]
-  qtopts << "with-mysql" if build.with?("mysql")
-  qtopts << "with-postgresql" if build.with?("postgresql")
-  depends_on "qt5" => qtopts
+  qt5_build_options = []
+  qt5_build_options << "with-oci" if build.with?("oci") && ENV["ORACLE_HOME"]
+  qt5_build_options << "with-mysql" if build.with?("mysql")
+  qt5_build_options << "with-postgresql" if build.with?("postgresql")
+  depends_on "qt5" => qt5_build_options
 
   def install
     args = ["--prefix=#{prefix}"]
-
-    if build.with? "debug"
-      args << "--enable-debug"
-    end
-
-    if build.with? "gui"
-      args << "--enable-gui-mod"
-    end
-
-    if build.with? "mongo"
-      args << "--enable-mongo"
-    end
+    args << "--enable-gui-mod" if build.with? "qt5"
 
     system "./configure", *args
 
@@ -49,17 +42,14 @@ class Treefrog < Formula
   end
 
   test do
-    cd testpath do
-      system "tspawn", "new", "hello"
-      assert File.exist?("hello")
-      cd "hello" do
-        assert File.exist?("hello.pro")
-        system HOMEBREW_PREFIX/"opt/qt5/bin/qmake"
-        assert File.exist?("Makefile")
-        system "make"
-        system "treefrog", "-d", "-e", "dev"
-        system "treefrog", "-k", "stop"
-      end
+    system bin/"tspawn", "new", "hello"
+    assert File.exist?("hello")
+    cd "hello" do
+      assert File.exist?("hello.pro")
+      system HOMEBREW_PREFIX/"opt/qt5/bin/qmake"
+      assert File.exist?("Makefile")
+      system "make"
+      system bin/"treefrog", "-v"
     end
   end
 end
