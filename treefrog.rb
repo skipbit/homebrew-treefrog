8add8857b9cfac05e4fd9605cbd487ca174d976f
Yuma Endo <endo@skipbit.jp>
2016-11-11 18:27:53 +0900

    Update options.
    - add debug option
    - rename qt-gui option

--- treefrog.rb
+++ treefrog.rb
@@ -5,28 +5,25 @@ class Treefrog < Formula
   sha256 "850744fef5254a8b2de1d949ba3490514ce1095e9a3e81da40aa8160129e14a1"
   head "https://github.com/treefrogframework/treefrog-framework.git", :branch => "master"
 
-  bottle do
-    sha256 "8d5daeb95fabb1bc5d0821cd4a29414b1cbc2d5483b094dd8d8a435172028685" => :sierra
-    sha256 "8d5daeb95fabb1bc5d0821cd4a29414b1cbc2d5483b094dd8d8a435172028685" => :el_capitan
-  end
-
+  option "with-debug", "build with debugging information"
   option "with-mysql", "enable --with-mysql option for Qt build"
   option "with-oci", "enable --with-oci option for Qt build"
   option "with-postgresql", "enable --with-postgresql option for Qt build"
-  option "with-qt5", "build and link with QtGui module"
+  option "with-qt-gui", "build and link with QtGui module"
 
   depends_on :macos => :el_capitan
   depends_on :xcode => [:build, "8.0"]
 
   qt5_build_options = []
-  qt5_build_options << "with-oci" if build.with?("oci") && ENV["ORACLE_HOME"]
   qt5_build_options << "with-mysql" if build.with?("mysql")
+  qt5_build_options << "with-oci" if build.with?("oci")
   qt5_build_options << "with-postgresql" if build.with?("postgresql")
   depends_on "qt5" => qt5_build_options
 
   def install
     args = ["--prefix=#{prefix}"]
-    args << "--enable-gui-mod" if build.with? "qt5"
+    args << "--enable-debug" if build.with? "debug"
+    args << "--enable-gui-mod" if build.with? "qt-gui"
 
     system "./configure", *args
 
